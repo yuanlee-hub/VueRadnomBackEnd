3fd984446c2f41553953a3ce361fae93ad348d31
liyuan <liyuan520310@163.com>
2020-05-28 22:25:00 +0800

    cookie

--- routes/api/v1/cookie.js
+++ routes/api/v1/cookie.js
@@ -1,11 +1,19 @@
 const router = require('express').Router()
 
 router.get('/', async(req, res, next) => {
+  const token = Date.now()
+  res.cookie('token', token)
+  res.cookie('expires', '2020-06-25')
+  res.cookie('maxAge', 3600 * 24)
+  res.cookie('test-1', 'test-1')
+  res.cookie('test-2', 'test-2')
+  res.cookie('Object', JSON.stringify({name:'Tom',age:20}))
+
   res.send({
     code: 0,
     data: {
       name: 'Cookie',
-      token: Date.now()
+      token
     }
   })
 })
