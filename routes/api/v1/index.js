0789c5dc469871864c33ccb3b198be56774564c1
leeyuan <liyuan520310@163.com>
2020-08-21 22:35:24 +0800

    Log

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,11 +3,12 @@ const cookieParser = require('cookie-parser')
 
 // cookies
 router.use(cookieParser(), async(req, res, next) => {
-    console.log('\n\n------------',new Date().toLocaleString())
+    console.log('\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>',new Date().toLocaleString())
     console.warn(req.method, req.url)
     console.warn('req.headers:', JSON.stringify(req.headers, null, 2))
     console.warn('req.cookies:', JSON.stringify(req.cookies, null, 2))
     console.warn('req.query:', JSON.stringify(req.query, null, 2))
+    console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
     next()
 })
 
